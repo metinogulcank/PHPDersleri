f97bff08a891c5cb366e4d01b0824670cdd8f772
metinogulcank <metinogulcank06@gmail.com>
2025-02-17 22:08:46 +0300

    Adding All Lessons

---
+++ kosulluifadeler.php
@@ -0,0 +1,20 @@
+<?php 
+
+// Mantıksal Operatörler
+// && (ve) operatörü
+// || (veya) operatörü
+// ! (değil) operatörü
+// == (eşit) operatörü
+// === (denk) operatörü
+
+$sayi = "101";
+if ($sayi %2 == 0){
+    echo "Sayı, ".$sayi." çifttir.";
+    echo "<br>";
+}
+else{
+    echo "Sayı, ".$sayi." tektir.";
+    echo "<br>";
+}
+
+?>
---
+++ operatorler.php
@@ -0,0 +1,12 @@
+<?php 
+// Aritmetik Operatörler
+$x = 10;
+$y = 20;
+echo $x + $y;
+echo "<br>";
+echo $y - $x;
+echo "<br>";
+echo $y * $x;
+echo "<br>";
+echo $y / $x;
+?>
